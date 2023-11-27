655ee4fcac25e5174695b995a296dfe1227c2a99
Andrey Mezentsev <mezentsew@mail.ru>
2023-11-28 00:35:08 +0300

    Create class Item in module main.py.

--- src/item.py
+++ src/item.py
@@ -2,6 +2,7 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
+
     pay_rate = 1.0
     all = []
 
@@ -13,7 +14,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +25,10 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity * self.pay_rate
 
-    def apply_discount(self) -> None:
+    def apply_discount(self, discount) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= 1 - discount / 100

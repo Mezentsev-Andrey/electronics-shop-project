f5b6e10b406e7b9a2de7b70196e5745cc7d3591e
Andrey Mezentsev <mezentsew@mail.ru>
2023-11-29 21:32:00 +0300

    Add new methods to module item.py.

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+import csv
+import math
+from typing import Any
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -9,21 +14,37 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
+        :param name: название товара.
+        :param price: цена за единицу товара.
+        :param quantity: количество товара в магазине.
         """
-        self.name = name
+        self._Item__name = None
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
+
+    @property
+    def name(self) -> str:
+        """
+        Геттер для получения значения приватного атрибута name.
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, new_name: str) -> None:
+        """
+        Сеттер для установки значения приватного атрибута name.
+        Проверяет длину наименования товара и обрезает, если необходимо.
+        """
+        if len(new_name) > 10:
+            self.__name = new_name[:10]
+        else:
+            self.__name = new_name
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
+        :return: общая стоимость товара.
         """
         return self.price * self.quantity * self.pay_rate
 
@@ -32,3 +53,28 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= 1 - discount / 100
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path: Any) -> None:
+        """
+        Инициализирует экземпляры класса Item данными из файла CSV.
+        :param file_path: путь к CSV-файлу.
+        """
+        cls.all.clear()
+        with open(file_path, encoding="windows-1251") as f:
+            reader = csv.DictReader(f, delimiter=",")
+            for row in reader:
+                name = row["name"]
+                price = Item.string_to_number(row["price"])
+                quantity = int(row["quantity"])
+                item = Item(name, price, quantity)
+                cls.all.append(item)
+
+    @staticmethod
+    def string_to_number(string: str):
+        """
+        Статический метод, возвращающий число из числа-строки.
+        :param string: число в виде строки.
+        :return: преобразованное число.
+        """
+        return int(math.floor(float((string.replace(",", ".")))))
